f6a9ffaa4c4e18b78c17d891e6317ef82d89316f
Akif Nuri Demir <32599513+AkifNDemir@users.noreply.github.com>
2020-10-23 07:17:53 +0300

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,35 @@
+<?php
+
+if(!file_exists(date("Y") . '/' . date("m"))) {
+    mkdir(date("Y") . '/' . date("m"));
+}
+if(!file_exists(date("Y") . '/' . date("m"). '/'. date("d"))) {
+    mkdir(date("Y") . '/' . date("m"). '/'. date("d"));
+}
+
+$dizin = date("Y") . '/' . date("m") . '/'. date("d") . '/';
+
+$dosyaAdi = date("H-i-s") . '_' . rand(0000000000,9999999999) . '.jpg';
+$yuklenecek_dosya = $dizin . $dosyaAdi;
+$url = "https://127.0.0.1/D:/Fivem/Servers/fivem-img/img/" . $dizin; // sonuna / koymanız gerekiyor. -- you have to put / at the end.
+
+
+if (move_uploaded_file($_FILES['files']['tmp_name'][0], $yuklenecek_dosya))
+{
+    $result = [
+        "success" => true,
+        "files" => [
+                [
+                    "hash" => $dosyaAdi,
+                    "name" => $dosyaAdi,
+                    "url" => $url . $dosyaAdi,
+                    "size" => $_FILES['files']['size'][0]
+                ]
+        ],
+
+    ];
+    print_r(json_encode($result));
+} else {
+    echo "Dosya yüklenemedi!\n";
+}
+?>
